5999850ac6c0856072ff2dbc6bd000b6221f937c
RazorHollow <rob@razorhollow.com>
2023-12-22 10:53:51 -0500

    add form ui

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Form } from "@remix-run/react";
+
+
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,33 +12,22 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="flex flex-col h-screen justify-center items-center bg-gradient-to-r from-blue-500 to-blue-300">
+      <h1 className="text-white text-4xl mb-36">To-Do List</h1>
+      <div className="w-1/2 h-1/2">
+        <Form method="post" navigate={false} className="flex">
+          <input name="todo" type="text" className="focus:outline-none w-1/2"></input>
+          <div className="bg-white">
+            <button className="bg-red-600 text-xl text-white px-2 hover:bg-red-500">+</button>
+          </div>
+          <select name="category" id="category" className="ml-6 focus:outline-none">
+            <option value="">Category</option>
+            <option value="work">work</option>
+            <option value="home">home</option>
+            <option value="shopping">shopping</option>
+          </select>
+        </Form>
+      </div>
     </div>
   );
 }
